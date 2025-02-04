5b9b9da22441e6f02fad624f32e0737968a39466
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-02-05 00:15:14 +0100

    chore(ffmpeg): wip

--- src/consumers/step4/step4.consumer.ts
+++ src/consumers/step4/step4.consumer.ts
@@ -1,20 +1,35 @@
 import { Processor, WorkerHost } from "@nestjs/bullmq";
-import { Inject } from "@nestjs/common";
+import { Inject, Logger } from "@nestjs/common";
 import { Job } from "bullmq";
 import { STEPS } from "../constant";
 
 @Processor('step4')
 export class Step4Consumer extends WorkerHost {
+    private readonly logger = new Logger(Step4Consumer.name);
+    
+    private readonly STEP2 = STEPS["3"];
+    private readonly STEP3 = STEPS["4"];
 
     constructor(@Inject("REDIS_CLIENT") private redisClient) {
         super();
     }
 
     async process(job: Job, token?: string): Promise<any> {
-        console.log("JOB STEP 4");
 
-        await job.updateProgress(100/STEPS.TOTAL);
-        return Promise.resolve();
+        try {
+            const rawCompletion = JSON.parse(await this.redisClient.getdel(`${job.data.jobUUID}-${this.STEP2.REDIS_KEY_RESULT}`));
+            
+            console.log(JSON.stringify(rawCompletion));
+
+            await job.updateProgress(100/STEPS.TOTAL);
+            return Promise.resolve();
+        } catch ( e ) {
+            this.logger.error(e);
+            //await job.moveToFailed({ message: e.message }, true);
+            return Promise.resolve();
+        }
+
+
     }
 
 }
