8d3cfd6784466b4ca21e888d7d297ebf755788ba
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-03-19 00:44:20 +0100

    ok

--- test.ts
+++ test.ts
@@ -1,3 +1,5 @@
+import { partition } from "rxjs";
+
 const fs = require('fs');
 const path = require('path');
 const ffmpeg = require('fluent-ffmpeg');
@@ -31,27 +33,78 @@ const wordWeight = total / punctuationWithoutSpaceText.length
 console.log(punctuationWithoutSpaceText)
 console.log(wordWeight)
 
+let partitions: Array<Array<string>> = [];
+let tmp: string[] =  [];
+punctuationWithoutSpaceText.forEach((word, index) => {
+    if ( index !== 0 && index % 3 === 0 || index === punctuationWithoutSpaceText.length - 1 ) {
+        partitions = [...partitions, tmp];
+        tmp = [];
+    }
+    tmp = [...tmp, word];
+});
+
+let complexFilter:Record<string,any>[] = partitions.map(( partition, index) => {
+    const text:string = partition.join(" ");
+    let options = {
+        fontfile: "D\\\\:/Dev/workspace/autopublisher-backend/assets/the_bold_font.ttf",
+        text,
+        fontsize: 80,
+        fontcolor: 'white',
+        x: '(w-text_w)/2',
+        y: 'h-text_h-200',
+        enable: 'between(t,0.9399999976158142, 2.939999997615814)', // TODO
+        box: 1,
+        boxcolor: 'black@1',
+        boxborderw: 10
+    }
+
+    let filter = {
+        filter: "drawtext",
+        options
+    };
+
+    if ( index === 0 ) {
+        filter["outputs"] = `outputs${index}`;
+    }
+
+    if ( index !== 0 && index !== partitions.length - 1 ) {
+        filter["inputs"] = `outputs${index - 1}`;
+        filter["outputs"] = `outputs${index}`;
+    }
+
+    if ( index === partitions.length - 1 ) {
+        filter["inputs"] = `outputs${index - 1}`;
+    }
+
+    return filter;
+})
+console.log(complexFilter);
+
+
 ffmpeg(path.resolve(process.cwd(),"fixtures", "Se lever tôt ne te rendra pas meilleur (et c'est tant mieux).webm"))
     .setStartTime(0)
     .setDuration(4)
-    .complexFilter([
-        {
-          filter: 'drawtext',
-          options: {
-            fontfile: "D\\\\:/Dev/workspace/autopublisher-backend/assets/the_bold_font.ttf",
-            text: "SUPER",
-            fontsize: 80,
-            fontcolor: 'white',
-            x: '(w-text_w)/2',
-            y: 'h-text_h-200',
-            enable: 'between(t,0.9399999976158142, 2.939999997615814)',
-            box: 1,
-            boxcolor: 'black@1',
-            boxborderw: 10
-          },
-        //   outputs: 'filter1' // [0:v] => filter1
-        }
-    ])
+    .complexFilter(
+    //     [
+    //     {
+    //       filter: 'drawtext',
+    //       options: {
+    //         fontfile: "D\\\\:/Dev/workspace/autopublisher-backend/assets/the_bold_font.ttf",
+    //         text: "SUPER",
+    //         fontsize: 80,
+    //         fontcolor: 'white',
+    //         x: '(w-text_w)/2',
+    //         y: 'h-text_h-200',
+    //         enable: 'between(t,0.9399999976158142, 2.939999997615814)',
+    //         box: 1,
+    //         boxcolor: 'black@1',
+    //         boxborderw: 10
+    //       },
+    //     //   outputs: 'filter1' // [0:v] => filter1
+    //     }
+    // ]
+
+)
     .on('end', function() {
         console.log('Finished processing');
         // resolve(true);
@@ -62,3 +115,5 @@ ffmpeg(path.resolve(process.cwd(),"fixtures", "Se lever tôt ne te rendra pas me
     })
     .output(path.resolve(process.cwd(), "fixtures", `TEST.webm`))
     .run();
+
+//  npx tsx .\test.ts
