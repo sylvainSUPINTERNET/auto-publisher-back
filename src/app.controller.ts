ca2af0296fb344ca98c7a232befc8da6e0574a0a
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-04-07 22:27:05 +0200

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+
+  @Post()
+  async create(@Body() createCatDto: any): Promise<string> {
+    return 'This action adds a new cat';
+  }
 
   @Get()
   getHello(): string {
